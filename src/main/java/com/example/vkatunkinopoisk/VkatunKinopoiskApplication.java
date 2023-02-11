fd854d3a031a9f225594eca6f14b3d7b30f913f5
karasique69 <karasiq69@gmail.com>
2023-02-11 04:54:30 +0300

    Create spring boot project

---
+++ src/main/java/com/example/vkatunkinopoisk/VkatunKinopoiskApplication.java
@@ -0,0 +1,13 @@
+package com.example.vkatunkinopoisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkatunKinopoiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkatunKinopoiskApplication.class, args);
+    }
+
+}
